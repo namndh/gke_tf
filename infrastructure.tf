ec37499d7a2d670c719326d7f05e438f03b9a44e
namndh <hainam.nguyen.ict@gmail.com>
2021-05-17 16:30:38 +0700

    update

--- infrastructure.tf
+++ infrastructure.tf
@@ -1,12 +1,5 @@
 variable "project" {}
 
-variable "postgres_user" {
-  default = "airflow"
-}
-variable "postgres_pw" {
-  default = "airflow"
-}
-
 variable "region" {
   default = "asia-southeast1"
 }
@@ -25,33 +18,33 @@ resource "google_compute_global_address" "airflow-static-ip" {
   name = "airflow-static-ip"
 }
 
-resource "google_compute_disk" "airflow-redis-disk" {
-  name  = "airflow-redis-disk"
-  type  = "pd-ssd"
-  size = "200"
-  zone  = "${var.zone}"
-}
+# resource "google_compute_disk" "airflow-redis-disk" {
+#   name  = "airflow-redis-disk"
+#   type  = "pd-ssd"
+#   size = "200"
+#   zone  = "${var.zone}"
+# }
 
-resource "google_sql_database_instance" "airflow-db" {
-  name = "airflow-db"
-  database_version = "POSTGRES_9_6"
-  region = "${var.region}"
-  settings {
-    tier = "db-g1-small"
-  }
-}
+# resource "google_sql_database_instance" "airflow-db" {
+#   name = "airflow-db"
+#   database_version = "POSTGRES_9_6"
+#   region = "${var.region}"
+#   settings {
+#     tier = "db-g1-small"
+#   }
+# }
 
-resource "google_sql_database" "airflow-schema" {
-  name = "airflow"
-  instance = "${google_sql_database_instance.airflow-db.name}"
-}
+# resource "google_sql_database" "airflow-schema" {
+#   name = "airflow"
+#   instance = "${google_sql_database_instance.airflow-db.name}"
+# }
 
-resource "google_sql_user" "proxyuser" {
-  name = "${var.postgres_user}"
-  password = "${var.postgres_pw}"
-  instance = "${google_sql_database_instance.airflow-db.name}"
-  host = "cloudsqlproxy~%"
-}
+# resource "google_sql_user" "proxyuser" {
+#   name = "${var.postgres_user}"
+#   password = "${var.postgres_pw}"
+#   instance = "${google_sql_database_instance.airflow-db.name}"
+#   host = "cloudsqlproxy~%"
+# }
 
 resource "google_compute_subnetwork" "gke-subnet" {
   name = "gkesubnet"
